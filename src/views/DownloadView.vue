912bfa9e4e97aabe242737c17c3f661ce7aa62b7
Connor deBoer <connor.deboer04@gmail.com>
2024-08-05 16:33:55 -0600

    Got the download page up with a dummy build, can't properly test it without temporarily setting the site live, but I'm gonna fix the colours and make it more responsive before I do that cause I'll need maggie's approval for the About BBG page

--- src/views/DownloadView.vue
+++ src/views/DownloadView.vue
@@ -1,18 +1,34 @@
 <script setup>
+import '../assets/explainer.css'
 </script>
 
 <template>
   <div class="main">
-    <p>Download Button</p>
+    <a href="/Build/Build.apk">
+      <div class="game-logo"></div>
+    </a>
+    <p>↑↑ click the image above to download the game ↑↑</p>
   </div>
 </template>
 
 <style scoped>
-.main
+.game-logo
 {
-    margin-top: 200px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  animation-name: grow-shrink;
+  animation-duration: 2.5s;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-in-out;
+}
+
+@keyframes grow-shrink
+{
+  0% { transform: scale(1); }
+  50% { transform: scale(1.05); }
+  100% { transform: scale(1); }
+}
+
+a:hover
+{
+  cursor: pointer;
 }
 </style>
